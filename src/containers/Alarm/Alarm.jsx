dd350e30fb4b47851128f1b4792719dd67eeefb2
eduardobattisti <eduardobattisti10@gmail.com>
2021-05-10 15:19:39 -0300

    refactoring functions

--- src/containers/Alarm/Alarm.jsx
+++ src/containers/Alarm/Alarm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
 	FaPlay, FaPause, FaReplyAll, FaPlus, FaMinus,
 } from 'react-icons/fa';
@@ -15,53 +15,79 @@ const Alarm = () => {
 	const [seconds, setSeconds] = useState(timer.getSeconds().toLocaleString('pt-BR', {
 		minimumIntegerDigits: 2,
 	}));
-	const [sessionTime, setSessionTime] = useState('', '', '', '', 25, 0);
-	const [breakTime, setBreakTime] = useState('', '', '', '', 5, 0);
+	const [sessionTime, setSessionTime] = useState(new Date('', '', '', '', 25, 0));
+	const [breakTime, setBreakTime] = useState(new Date('', '', '', '', 5, 0));
 	const [isRunning, setIsRunning] = useState(false);
-	const [intervalId, setIntervalId] = useState(0);
+	// const [intervalId, setIntervalId] = useState(0);
 	// const [isBreakTime, setIsBreakTime] = useState(false);
 
-	onClickPlayPause = () => {
+	const onClickPlayPause = () => {
 		if (!isRunning) {
-			const runningTime = setInterval(() => {
-				setTimer(new Date('', '', '', '', '', timer.getSeconds() - 1));
-			}, 50);
-			setIntervalId(runningTime);
-		} else {
-			clearInteval(intervalId);
 			setIsRunning(true);
+		} else {
+			setIsRunning(false);
 		}
 	};
 
-	onClickSessionPlus = () => {
-		setSessionTime(new Date('', '', '', '', sessionTime.getMinutes() + 1, 0));
+	// Session Buttons
+	const onClickSessionPlus = () => {
+		if (sessionTime.getHours() === 1) {
+			setSessionTime(new Date('', '', '', '', sessionTime.getHours() * 60, 0));
+		} else {
+			setSessionTime(new Date('', '', '', '', sessionTime.getMinutes() + 1, 0));
+		}
 	};
 
-	onClickSessionMinus = () => {
-		setSessionTime(new Date('', '', '', '', sessionTime.getMinutes() - 1, 0));
+	const onClickSessionMinus = () => {
+		if (sessionTime.getMinutes() > 1 || sessionTime.getHours() === 1) {
+			setSessionTime(new Date('', '', '', '', sessionTime.getMinutes() - 1, 0));
+		}
 	};
 
-	onClickBreakPlus = () => {
-		setBreakTime(new Date('', '', '', '', breakTime.getMinutes() + 1, 0));
+	// Break Buttons
+	const onClickBreakPlus = () => {
+		if (breakTime.getHours() === 1) {
+			setBreakTime(new Date('', '', '', '', breakTime.getHours() * 60, 0));
+		} else {
+			setBreakTime(new Date('', '', '', '', breakTime.getMinutes() + 1, 0));
+		}
 	};
 
-	onClickBreakMinus = () => {
-		setBreakTime(new Date('', '', '', '', breakTime.getMinutes() - 1, 0));
+	const onClickBreakMinus = () => {
+		if (breakTime.getMinutes() > 1 || breakTime.getHours() === 1) {
+			setBreakTime(new Date('', '', '', '', breakTime.getMinutes() - 1, 0));
+		}
+	};
+
+	// Repeat Button
+	const onClickRepeat = () => {
+		// clearInterval(intervalId);
+		setTimer(new Date('', '', '', '', 25, 0));
+		setSessionTime(new Date('', '', '', '', 25, 0));
+		setBreakTime(new Date('', '', '', '', 5, 0));
 	};
 
 	useEffect(() => {
 		if (!isRunning) {
-			setTimer(new Date('', '', '', '', sessionTime.getMinutes(), 0));
+			if (sessionTime.getHours() === 1) {
+				setTimer(new Date('', '', '', '', sessionTime.getHours() * 60, 0));
+			} else {
+				setTimer(new Date('', '', '', '', sessionTime.getMinutes(), 0));
+			}
 		}
 	}, [sessionTime]);
 
 	useEffect(() => {
-		setMinutes(timer.getMinutes().toLocaleString('pt-BR', {
-			minimumIntegerDigits: 2,
-		}));
-		setSeconds(timer.getSeconds().toLocaleString('pt-BR', {
-			minimumIntegerDigits: 2,
-		}));
+		if (sessionTime.getHours() === 1) {
+			setMinutes('60');
+		} else {
+			setMinutes(timer.getMinutes().toLocaleString('pt-BR', {
+				minimumIntegerDigits: 2,
+			}));
+			setSeconds(timer.getSeconds().toLocaleString('pt-BR', {
+				minimumIntegerDigits: 2,
+			}));
+		}
 	}, [timer]);
 
 	return (
@@ -91,8 +117,8 @@ const Alarm = () => {
 						</p>
 					</Screen>
 					<div className="break">
-						<Button id="break-increment" className="plusMinusButton" onClick={onClickPlus}><FaPlus /></Button>
-						<Button id="break-decrement" className="plusMinusButton" onClick={onClickMinus}><FaMinus /></Button>
+						<Button id="break-increment" className="plusMinusButton" onClick={onClickBreakPlus}><FaPlus /></Button>
+						<Button id="break-decrement" className="plusMinusButton" onClick={onClickBreakMinus}><FaMinus /></Button>
 					</div>
 				</div>
 			</div>
@@ -105,7 +131,7 @@ const Alarm = () => {
 			</div>
 			<div className="timer">
 				<Screen className="screenTimer">
-					<h2 id="timer-label">{isBreakTime ? 'BREAK' : 'SESSION'}</h2>
+					<h2 id="timer-label">SESSION</h2>
 					<h1 id="time-left">{`${minutes}:${seconds}`}</h1>
 				</Screen>
 			</div>
